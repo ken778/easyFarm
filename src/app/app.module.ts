44e0be5ca411e6bbe60fdcd607213f0e8bf8c58c
kenn778 <kennethtumi18@gmail.com>
2021-06-08 15:51:50 +0200

    afternoon commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { FeaturesComponent } from './pages/features/features.component';
-
-
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    FeaturesComponent
+    FeaturesComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatGridListModule
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
